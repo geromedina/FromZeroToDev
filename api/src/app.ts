56fdc2dc6b95aec00f5a7d8b2d6de404b617069b
vraktos <matidemonte@hotmail.com>
2023-03-29 20:07:56 -0300

    Añadido next() a middleware en app.ts

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,21 +1,19 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Request, Response, NextFunction } from "express";
 
 const router = express.Router();
 const app = express();
-
 
 // MIDDLEWARES
 app.use(express.json());
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
-    res.status(500).send('Error interno del servidor');
-  });
-  
+  console.error(err.stack);
+  res.status(500).send("Error interno del servidor");
+  next();
+});
 
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('esta funcionando correctamente el get');
+app.get("/", (req: Request, res: Response) => {
+  res.send("esta funcionando correctamente el get");
 });
 
 export default app;
