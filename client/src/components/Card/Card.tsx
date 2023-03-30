fff874246824b467d692d229fecb99dbf32dc1d1
Alejandro Randazzo <alejandro.randazzo@hotmail.com>
2023-03-29 23:56:56 -0300

    Cards Container

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+/* import { NavLink } from "react-router-dom"; */
 import img from "../../assets/pictures/img.jpeg"
 
 interface CardProps {
--- client/src/ejemplo.ts
+++ client/src/ejemplo.ts
@@ -1,4 +1,4 @@
-export const currentCard = {
+export const currentCard = [{
     "_id": {
         "$oid": "6424dd601d4b9f6a3de0554b"
     },
@@ -29,4 +29,68 @@ export const currentCard = {
     "__v": {
         "$numberInt": "0"
     }
-}
+},
+{
+    "_id": {
+        "$oid": "6424dd601d4b9f6a3de0554b"
+    },
+    "name": "Introduction to JavaScript",
+    "description": "First course",
+    "user_id": {
+        "$oid": "6424dc63a6ae726175f9ecfc"
+    },
+    "image": "asdasd",
+    "difficulty": "Easy",
+    "duration": {
+        "$numberInt": "4"
+    },
+    "price": {
+        "$numberInt": "500"
+    },
+    "video": "asd",
+    "createdAt": {
+        "$date": {
+            "$numberLong": "1680137568332"
+        }
+    },
+    "updatedAt": {
+        "$date": {
+            "$numberLong": "1680137568332"
+        }
+    },
+    "__v": {
+        "$numberInt": "0"
+    }
+},
+{
+    "_id": {
+        "$oid": "6424dd601d4b9f6a3de0554b"
+    },
+    "name": "Introduction to JavaScript",
+    "description": "First course",
+    "user_id": {
+        "$oid": "6424dc63a6ae726175f9ecfc"
+    },
+    "image": "asdasd",
+    "difficulty": "Easy",
+    "duration": {
+        "$numberInt": "4"
+    },
+    "price": {
+        "$numberInt": "500"
+    },
+    "video": "asd",
+    "createdAt": {
+        "$date": {
+            "$numberLong": "1680137568332"
+        }
+    },
+    "updatedAt": {
+        "$date": {
+            "$numberLong": "1680137568332"
+        }
+    },
+    "__v": {
+        "$numberInt": "0"
+    }
+}]
