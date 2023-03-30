16a7c4fc774ef63c83916fe8614052241ac87cf4
vraktos <matidemonte@hotmail.com>
2023-03-30 19:11:55 -0300

    redux andando pero no terminado

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import CardsContainer from '../../components/CardsContainer';
+import React from "react";
+import CardsContainer from "../../components/CardsContainer";
+import { useAppDispatch } from "../../store/hooks";
+import { useEffect } from "react";
+import { getCourses } from "../../store/coursesSlices";
 
 const Home: React.FC = (): JSX.Element => {
-    return (
-        <>
-            <CardsContainer />
-        </>
-    )
-}
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getCourses());
+  }, []);
+  return (
+    <>
+      <CardsContainer />
+    </>
+  );
+};
 
-export default Home
+export default Home;
---
+++ client/src/store/coursesSlices.ts
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import type { AppThunk, RootState } from "./store";
+import axios from "axios";
+import { ThunkActionDispatch } from "redux-thunk";
+
+interface ICourse {
+  user_id: string;
+  name: string;
+  description: string;
+  image: string;
+  difficulty: string;
+  duration: number;
+  price: number;
+  video: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+interface CoursesState {
+  courses: ICourse[];
+}
+
+const initialState: CoursesState = {
+  courses: [],
+};
+
+export const coursesSlice = createSlice({
+  name: "courses",
+  initialState,
+  reducers: {
+    fetchCourses: (state, action: PayloadAction<ICourse[]>) => {
+      return { ...state, courses: action.payload };
+    },
+  },
+});
+export const getCourses = (): AppThunk => {
+  return async (dispatch) => {
+    const rawData = await axios.get("http://localhost:3001/courses");
+    const response = rawData.data;
+    console.log(response);
+    dispatch(fetchCourses(response));
+  };
+};
+export const { fetchCourses } = coursesSlice.actions;
+export default coursesSlice.reducer;
