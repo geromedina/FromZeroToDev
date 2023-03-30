745a813e991e361f8a15294ebae1e551e75aaf13
Ramiro <ramiroignacio_14@hotmail.com>
2023-03-30 18:52:59 -0300

    post validado usuario

--- api/src/handlers/usersHandlers.ts
+++ api/src/handlers/usersHandlers.ts
@@ -1,15 +1,29 @@
 import { Request, Response } from "express";
-import { getUsersContr } from "../controllers/usersControllers";
+import {
+  getUsersController,
+  createUser,
+} from "../controllers/usersControllers";
+import { IUser } from "../utils/types";
 
 export const getUsersHandler = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   try {
-    const response = await getUsersContr();
+    const response = await getUsersController();
     res.status(200).json(response);
   } catch (error: any) {
     console.log("hay una error");
+    res.status(400).json({ error });
+  }
+};
+//MANEJADOR QUE CREA LOS USUARIOS
+export const postUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const user = req.body as IUser;
+    const createdUser = await createUser(user);
+    res.status(200).json(createdUser);
+  } catch(error) {
     res.status(400).json({ error });
   }
 };
