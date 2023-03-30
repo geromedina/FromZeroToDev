2310c1be1d6b7e9f97ad20fb2851a5d7d7b0898f
geromedina <gerocode768@gmail.com>
2023-03-30 00:21:20 -0300

    ahorita sigo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,9 @@ import Home from './pages/Home/Home'
 
 const App: React.FC = (): JSX.Element => {
     return (
-        <Home/>
+        <div className='bg-blue-600'>
+            <Home/>
+        </div>
     )
 }
 
---
+++ client/src/components/NavBar/NavBar.ts
@@ -0,0 +1,31 @@
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
+// import lupa from "../../assets/pictures/lupa.jpg";
+
+// const NavBar: React.FC = () => {
+
+//   const location = useLocation();
+
+//     return (
+//     <nav>
+//       <div>
+//         <img src={lupa} alt="logo" />
+//         <ul>
+//           <li>
+//             <NavLink to="/home">
+//               Home
+//             </NavLink>
+//           </li>
+//           <li>
+//             {/* <NavLink to="/create">
+//               Create course
+//             </NavLink> */}
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default NavBar;
