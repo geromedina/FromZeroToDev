868f26c393a043d0f1f96d9482f71b7a30a639e6
geromedina <gerocode768@gmail.com>
2023-03-31 16:16:44 -0300

    xd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,8 +15,6 @@ const App: React.FC = (): JSX.Element => {
         <Routes>
           <Route exact path="/" element={<Landing />} />
           <Route path="/home" element={<Home />} />
-          {/* <Route exact path="/courses" element={<Courses />} />
-          <Route exact path="/create" element={<Form />} /> */}
         </Routes>
       </div>
     )
