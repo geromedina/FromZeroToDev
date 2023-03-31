bd67e2ba0b457345e10b5bc69ba95326cbe3e919
vraktos <matidemonte@hotmail.com>
2023-03-31 13:00:42 -0300

    form working missing styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
+import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-import Home from './pages/Home/Home'
-import NavBar from './components/NavBar';
-import Landing from './pages/Landing/Landing';
-import './index.css'
+import Home from "./pages/Home/Home";
+import NavBar from "./components/NavBar";
+import Landing from "./pages/Landing/Landing";
+import Form from "./pages/Form/Form";
+import "./index.css";
 
 const App: React.FC = (): JSX.Element => {
-    return (
-        <div>
-        {location.pathname !== "/" && <NavBar />}
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Landing />} />
-        </Routes>
-      </div>
-    )
-}
+  return (
+    <div>
+      {/* {location.pathname !== "/" && <NavBar />} */}
+      <Routes>
+        <Route path="/create" element={<Form />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
-
-
+export default App;
