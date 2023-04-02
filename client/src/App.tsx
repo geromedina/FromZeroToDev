d71174fe87c219588f4acba581d259ae9f195b5b
jorgenieva23 <jorge_4755@hotmail.com>
2023-04-02 17:48:40 -0300

    Courses

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import {Home, Landing, Form, Courses} from "./pages/index"
 import NavBar from "./components/NavBar";
-import Landing from "./pages/Landing/Landing";
-import Form from "./pages/Form/Form";
+
+
 import "./index.css";
 import CardDetail from "./components/CardDetail/CardDetail";
 
 const App: React.FC = (): JSX.Element => {
+  const location = useLocation()
   return (
     <div>
-      {/* {location.pathname !== "/" && <NavBar />} */}
+      {location.pathname !== "/" && <NavBar />}
       <Routes>
         <Route path="/create" element={<Form />} />
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Landing />} />
         <Route path= "/detail/:id" element= {<CardDetail/>}/>
+        <Route path="/" element={<Courses />} />
       </Routes>
     </div>
   );
--- client/src/components/CardsContainer/CardsContainer.tsx
+++ client/src/components/CardsContainer/CardsContainer.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import { useAppSelector } from "../../store/hooks";
 import Card from "../Card/Card";
+import { useSelector } from "react-redux";
 
-const CardsContainer: React.FC = (): JSX.Element => {
+
+const CardsContainer: React.FC = (currentCourses:any): JSX.Element => {
   const courses = useAppSelector((state) => state.courses.filteredCourses);
+  const allCourses:any  = useAppSelector((state)=> state.courses);
+
   return (
     <>
-      {courses.map((course) => (
+      {currentCourses.length ?
+      currentCourses && courses.map(course => (
         <Card
           name={course.name}
           difficults={course.difficulty}
           image={course.image}
-          id={course._id}
+          id="idDePrueba"
           description={course.description}
         />
-      ))}
+      ))
+      : !currentCourses.length && allCourses.length ?
+      <div>
+        <h2> No hay cursos </h2>
+      </div> :
+      <h2>Cargando..</h2>
+    }
     </>
   );
 };
