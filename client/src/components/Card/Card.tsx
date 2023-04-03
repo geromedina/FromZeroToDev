09df5278062885cad21d8ee2916ffaee8e5bdf21
Alejandro Randazzo <alejandro.randazzo@hotmail.com>
2023-04-03 00:33:06 -0300

    Conecté detail nuevamente. Modifiqué para que entre bien el Id

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -10,7 +10,7 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ name, difficults, id, image, description }) => {
-    
+    console.log(id)
     return (
 
             <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
