40039e3ce0cf64ade148f0fce8dbb079cb36ecc6
geromedina <gerocode768@gmail.com>
2023-04-17 16:18:43 -0300

    okis dokis

--- api/src/app.ts
+++ api/src/app.ts
@@ -6,17 +6,17 @@ import cors from "cors";
 const app = express();
 
 // Configuración de CORS
-const allowedOrigins = ["https://fromzerotodev-production.up.railway.app", "*"];
-const corsOptions: cors.CorsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// const allowedOrigins = ["https://fromzerotodev-production.up.railway.app", "*"];
+// const corsOptions: cors.CorsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
 // MIDDLEWARES
 
@@ -26,7 +26,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
