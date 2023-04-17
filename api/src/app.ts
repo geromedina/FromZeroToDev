ae488cac93a1592caa4c1bd4c75991250619e037
geromedina <gerocode768@gmail.com>
2023-04-17 16:09:23 -0300

    asdasd

--- api/src/app.ts
+++ api/src/app.ts
@@ -19,6 +19,13 @@ const corsOptions: cors.CorsOptions = {
 };
 
 // MIDDLEWARES
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(cors(corsOptions));
 
 app.use(express.json());
