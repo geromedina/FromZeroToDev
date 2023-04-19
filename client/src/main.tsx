3b360493afbec44619de89092e60c37bb5c0bdb3
geromedina <gerocode768@gmail.com>
2023-04-19 20:21:16 -0300

    dominio

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,23 +4,23 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import Auth0ProviderWithRedirectUri from "./components/Auth0Provider/auth0Provider";
-// export const frontURL= `https://fromzerotodev-front.vercel.app`;
-// export const backURL= `https://fromzerotodev-production.up.railway.app`
-export const frontURL = `http://localhost:3000`;
-export const backURL = `http://localhost:3001`;
+import Auth0ProviderWithRedirectUri from "./components/Auth0Provider/auth0Provider"; 
+export const frontURL= `https://fromzerotodev-front.vercel.app`;
 
+/* export const frontURL= http://localhost:3000 /
+/ export const backURL= http://localhost:3001 */
+
+export const backURL= `https://fromzerotodev-production.up.railway.app`
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <Auth0ProviderWithRedirectUri
-          domain="dev-vvztjksljjvfv0t3.us.auth0.com"
-          clientId="7hAv7GGHCbBdukK6oZkDWczJEcJaDISY"
-          redirectUri={frontURL}
-        >
-          <App />
-        </Auth0ProviderWithRedirectUri>
+          <Auth0ProviderWithRedirectUri 
+          domain='dev-c556ut7dezqsu5xn.us.auth0.com'
+          clientId='Pt4iDYS5IXt3Bufu5g1aBA2NH0EkOd0h'
+          redirectUri={frontURL}>
+            <App />
+          </Auth0ProviderWithRedirectUri>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>
